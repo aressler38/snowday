58d664482de7304912266fce5277dd5a79bed205
alexander <alexander.ressler@gmail.com>
2021-02-20 22:06:30 -0500

    handle potato gun rotation

--- snowday/game.py
+++ snowday/game.py
@@ -4,6 +4,7 @@ This is the game class
 import sys
 import math
 import pygame
+from pygame.math import Vector2
 import pygame.sprite
 import pygame.mouse
 from snowday.bgm import Bgm
@@ -30,23 +31,19 @@ class Game:
         self.displaysurface = pygame.display.set_mode((Game.WIDTH, Game.HEIGHT))
         self.bgm = Bgm()
 
-    def bgm_on(self):
-        """ Play the background music """
-        self.bgm.play()
-
     def get_gun_angle(self):
+        """
+        Calculate the angle the potato gun should point based on the mouse cursor.
+        """
         vec2 = pygame.mouse.get_pos()
         y = Game.HEIGHT - vec2[1]
         x =  vec2[0] - Game.WIDTH / 2
-
         angle = int(math.atan2(y, x) * 180 / math.pi) - 90
-        print(angle)
         return angle
-
 
     def run(self):
         """ Run the game loop """
-        self.bgm_on()
+        self.bgm.play()
 
         snowflakes = pygame.sprite.Group()
 
@@ -60,7 +57,10 @@ class Game:
         snowflake = Snowflake()
         snowflakes.add(snowflake)
 
-        potato_gun = PotatoGun((Game.WIDTH / 2, Game.HEIGHT))
+        potato_gun = PotatoGun(Vector2(Game.WIDTH / 2, Game.HEIGHT))
+
+        # Keep track of when the player finishes clicking a mouse button.
+        click_start = False
 
         while True:
             for event in pygame.event.get():
@@ -69,27 +69,38 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
+            # Draw a black background.
             self.displaysurface.fill((0, 0, 0))
+            # Draw the ground.
+            self.displaysurface.blit(platform.image, platform.rect)
 
+            # Update the snowflakes sprite group causing them to fall.
             snowflakes.update()
-            snowflakes.draw(self.displaysurface)
-
-            #for sprite in snowflakes:
-            #    self.displaysurface.blit(sprite.image, sprite.rect)
-
+            # Check if the snowflake hit the ground.
             for snowflake in pygame.sprite.spritecollide(platform, snowflakes, True):
                 print('melt')
                 snowflake = Snowflake()
                 snowflakes.add(snowflake)
+            # Draw the snowflakes that didn't hit the ground.
+            snowflakes.draw(self.displaysurface)
 
-
-            self.displaysurface.blit(platform.image, platform.rect)
-
+            # Calculate the potato gun angle based on the mouse cursor.
             angle = self.get_gun_angle()
-            potato_gun.rotate(angle, (Game.WIDTH / 2, Game.HEIGHT))
-            potato_gun.rect.y -= potato_gun.rect.height / 2
+            # Aim the gun.
+            potato_gun.rotate(angle)
+            # Draw the potato gun
             self.displaysurface.blit(potato_gun.image, potato_gun.rect)
 
+            # See if the player finished clicking a shoot button.
+            for mouse_btn in pygame.mouse.get_pressed(3):
+                if mouse_btn:
+                    click_start = True
+                    break
+                if click_start:
+                    potato_gun.shoot_potato()
+                    click_start = False
+
+            # Update the display and clock.
             pygame.display.update()
             self.clock.tick(Game.FPS)
 
--- snowday/sprites/potato_gun.py
+++ snowday/sprites/potato_gun.py
@@ -2,6 +2,7 @@ from typing import Tuple
 import pkg_resources
 
 import pygame
+from pygame.math import Vector2
 import pygame.sprite
 import pygame.image
 import pygame.transform
@@ -9,7 +10,7 @@ import pygame.transform
 
 class PotatoGun(pygame.sprite.Sprite):
 
-    def __init__(self, center: Tuple):
+    def __init__(self, pivot: Vector2):
         super().__init__()
         pth = pkg_resources.resource_filename(
             'snowday',
@@ -17,13 +18,33 @@ class PotatoGun(pygame.sprite.Sprite):
         surface = pygame.image.load(pth)
         rect = surface.get_rect()
         factor = 4
+        self.potatoes = pygame.sprite.Group()
+        self.pivot = pivot
         self.original = pygame.transform.scale(surface, (int(rect.width / factor), int(rect.height / factor)))
-        self.original.get_rect().center = center
+        self.original_rect = self.original.get_rect()
+        self.original_rect.center = (pivot.x, pivot.y - self.original_rect.height / 2)
+        self.original_center_vec = Vector2(self.pivot - self.original_rect.center)
+        print(self.original_rect.center)
         self.image = self.original
         self.rect = self.image.get_rect()
         self.angle = 0
 
-    def rotate(self, angle: float, pivot):
+
+    def rotate(self, angle: float):
+        """
+        Rotate the potato gun about the pivot point.
+        """
+        # TODO: If I was smart, then I'd position the original image such that I wouldn't need
+        # to rotate two vectors. I should just be able to rotate the one vector.
+        rotated_image = pygame.transform.rotate(self.original, angle)
+        floating_center = self.original_center_vec.rotate(angle)
+        center = (floating_center.x + self.pivot.x, self.pivot.y - floating_center.y)
+        self.rect = rotated_image.get_rect(center=center)
+        self.image = rotated_image
         self.angle = angle
-        self.image = pygame.transform.rotate(self.original, self.angle)
-        self.rect.center = pivot
+
+    def shoot_potato(self):
+        """
+        Fire!!!
+        """
+        print('potato')
