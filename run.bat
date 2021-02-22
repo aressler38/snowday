88a735ac92bee7da60cf2c5308d2e0872e785649
alexander <alexander@brainslavestudios.com>
2021-02-22 08:41:06 -0500

    fixing windows install hopefully

--- run.bat
+++ run.bat
@@ -1,6 +1,10 @@
-python -m pip check
+
+pip show --files snowday
+
 IF %ERRORLEVEL% NEQ 0 (
+    echo "I am going to try and install the game now."
     pip install .
 )
+
 echo "running the game..."
 python snowday\main.py
